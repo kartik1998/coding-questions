3de5744e53ff5f06d83344586582307d13dde308
Kartik <kartikrawat61@gmail.com>
2019-08-18 16:55:48 +0530

    Code

--- src/Divide_And_Conquer/Aggressive_Cows.java
+++ src/Divide_And_Conquer/Aggressive_Cows.java
@@ -7,6 +7,9 @@ import java.lang.*;
 public class Aggressive_Cows {
     public static boolean check(int A[],int cows,int d){
         int count=1;
+        if(count==cows){
+            return true;
+        }
         int j=0;
         for(int i=1;i<A.length;i++){
             if(A[i]-A[j]>=d){
@@ -19,39 +22,34 @@ public class Aggressive_Cows {
         }
         return false;
     }
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int t=0,n=0,cows=0;
+        int n=0,cows=0;
         if(sc.hasNext()){
-            t=sc.nextInt();
+            n=sc.nextInt();
         }
-        while(t-->0){
-            if(sc.hasNext()){
-                n=sc.nextInt();
-            }
-            if(sc.hasNext()){
-                cows=sc.nextInt();
-            }
-            int A[]=new int[n];
-            for(int i=0;i<n;i++){
-                if(sc.hasNext()) {
-                    A[i] = sc.nextInt();
-                }
-            }
-            Arrays.sort(A);
-            int s=0;
-            int e=A[n-1]-A[0];
-            int ans=-1;
-            while(s<=e){
-                int mid=(s+e)/2;
-                if(check(A,cows,mid)){
-                    ans=Math.max(ans,mid);
-                    s=mid+1;
-                }else{
-                    e=mid-1;
-                }
-            }
-            System.out.println(ans);
+        if(sc.hasNext()){
+            cows=sc.nextInt();
         }
+        int A[]=new int[n];
+        for(int i=0;i<n;i++){
+            if(sc.hasNext()){
+                A[i]=sc.nextInt();
+            }
+        }
+        Arrays.sort(A);
+        int s=0;
+        int e=A[n-1];
+        int ans=-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(check(A,cows,mid)){
+                ans=Math.max(ans,mid);
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
+        }
+        System.out.println(ans);
     }
 }
