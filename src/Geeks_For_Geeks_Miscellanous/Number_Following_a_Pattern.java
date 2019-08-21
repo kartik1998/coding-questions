5f7d3604aff58d32082fa98e411006815f26b627
Kartik <kartikrawat61@gmail.com>
2019-08-21 20:24:41 +0530

    Coding

---
+++ src/Geeks_For_Geeks_Miscellanous/Number_Following_a_Pattern.java
@@ -0,0 +1,27 @@
+package Geeks_For_Geeks_Miscellanous;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Number_Following_a_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            String str=sc.next();
+            Stack<Integer> s = new Stack<>();
+            int num=1;
+            s.push(1);
+            for(int i=0;i<=str.length();i++){
+                num++;
+                if(i==str.length() || str.charAt(i)=='I'){
+                    while(!s.isEmpty()){
+                        System.out.print(s.pop());
+                    }
+                }
+                s.push(num);
+            }
+            System.out.println();
+        }
+    }
+}
