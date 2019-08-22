387bbe36d17832a3b89abcb00b8e69daf766d56e
Kartik <kartikrawat61@gmail.com>
2019-08-22 20:07:12 +0530

    coding

--- src/Graphs/Snake_And_Ladder.java
+++ src/Graphs/Snake_And_Ladder.java
@@ -1,60 +1,49 @@
 package Graphs;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-class pair{
+class Pair{
     int x,y;
-    pair(int x,int y){
+    Pair(int x,int y){
         this.x=x;
         this.y=y;
     }
 }
 public class Snake_And_Ladder {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
         while(t-->0){
             int n=sc.nextInt();
-            HashMap<Integer,Integer> map=new HashMap<>();
+            int A[]=new int[31];
             for(int i=0;i<n;i++){
-                int k=sc.nextInt();
-                int v=sc.nextInt();
-                map.put(k,v);
+                A[sc.nextInt()]=sc.nextInt();
             }
             boolean visited[]=new boolean[31];
-            int ans=-1;
-            Queue<pair> q = new LinkedList<>();
-            q.add(new pair(1,0));
+            Queue<Pair> q = new LinkedList<>();
+            q.add(new Pair(1,0));
             while(!q.isEmpty()){
-                pair p=q.remove();
-                int x=p.x;
-                int y=p.y;
-                for(int i=1;i<=6;i++){
-                    int X=x+i;
-                    if(X<31 && map.containsKey(X)){
-                        if(!visited[map.get(X)]){
-                            visited[map.get(X)]=true;
-                            q.add(new pair(map.get(X),y+1));
+                Pair p=q.remove();
+                for(int x=p.x+1;x<=p.x+6;x++){
+                    if(x< 31 && A[x]==0){
+                        if(!visited[x]){
+                            q.add(new Pair(x,p.y+1));
+                            visited[x]=true;
                         }
-                    }else{
-                        if(X<31 && !visited[X]){
-                            visited[X]=true;
-                            q.add(new pair(X,y+1));
+                    }else if(x<31){
+                        if(!visited[A[x]]){
+                            q.add(new Pair(A[x],p.y+1));
+                            visited[A[x]]=true;
                         }
-                    }
-                    if(visited[30]){
-                        ans=y+1;
-                        break;
                     }
                 }
-                if(ans!=-1){
+                if(visited[30]){
+                    int ans=p.y+1;
                     System.out.println(ans);
                     break;
                 }
             }
-
         }
     }
 }
