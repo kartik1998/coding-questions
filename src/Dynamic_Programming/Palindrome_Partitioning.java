08004306ad1b8e3d2f7ddd209f2c9c30e45c5c44
Kartik <kartikrawat61@gmail.com>
2019-08-26 20:28:59 +0530

    coding

--- src/Dynamic_Programming/Palindrome_Partitioning.java
+++ src/Dynamic_Programming/Palindrome_Partitioning.java
@@ -3,36 +3,36 @@ package Dynamic_Programming;
 import java.util.Scanner;
 
 public class Palindrome_Partitioning {
-    public static boolean check(char str[],int s,int e){
-        for(int i=s,j=e;i<j;i++,j--){
-            if(str[i]!=str[j]){
+    public static boolean check(String s,int l,int r){
+        for(int i=l,j=r;i<j;i++,j--){
+            if(s.charAt(i)!=s.charAt(j)){
                 return false;
             }
         }
         return true;
     }
-    public static int minCuts(char s[],int i,int j,Integer dp[][]){
-        if(i>=j || check(s,i,j)){
-            return 0;
+    public static int helper(String s,int l,int r,Integer dp[]){
+        if(check(s,l,r)){
+            return dp[l]=0;
         }
-        if(dp[i][j]!=null){
-            return dp[i][j];
+        if(dp[l]!=null){
+            return dp[l];
         }
         int min=Integer.MAX_VALUE;
-        for(int k=i;k<j;k++){
-            int sub_res=minCuts(s,i,k,dp)+1+minCuts(s,k+1,j,dp);
-            min=Math.min(sub_res,min);
+        for(int i=l;i<r;i++){
+            if(check(s,l,i)){
+                min=Math.min(1+helper(s,i+1,r,dp),min);
+            }
         }
-        return dp[i][j]=min;
+        return dp[l]=min;
     }
     public static void main (String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
         while(t-->0){
-            String str=sc.next();
-            char s[]=str.toCharArray();
-            Integer dp[][]=new Integer[s.length+1][s.length+1];
-            System.out.println(minCuts(s,0,s.length-1,dp));
+            String s=sc.next();
+            Integer dp[]=new Integer[s.length()+1];
+            System.out.println(helper(s,0,s.length()-1,dp));
         }
     }
 }
