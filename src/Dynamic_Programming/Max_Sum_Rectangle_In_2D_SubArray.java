6f9fc979c77ec16515a6e682412c3863f2164a5e
Kartik <kartikrawat61@gmail.com>
2019-08-28 17:08:16 +0530

    coding

--- src/Dynamic_Programming/Max_Sum_Rectangle_In_2D_SubArray.java
+++ src/Dynamic_Programming/Max_Sum_Rectangle_In_2D_SubArray.java
@@ -3,17 +3,17 @@ package Dynamic_Programming;
 import java.util.Scanner;
 
 public class Max_Sum_Rectangle_In_2D_SubArray {
-    public static int kadanes(int A[]){
+    public static int kadane(int A[]){
         int curr=0;
         int max=Integer.MIN_VALUE;
         for(int i=0;i<A.length;i++){
             curr+=A[i];
-            max=Math.max(curr,max);
+            max=Math.max(max,curr);
             curr=Math.max(curr,0);
         }
         return max;
     }
-    public static int sum(int A[][],int l,int r,int i){
+    public static int sum(int A[][],int i,int l,int r){
         int sum=0;
         for(int j=l;j<=r;j++){
             sum+=A[i][j];
@@ -22,28 +22,24 @@ public class Max_Sum_Rectangle_In_2D_SubArray {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int A[][] = new int[n][m];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    A[i][j] = sc.nextInt();
-                }
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int A[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                A[i][j]=sc.nextInt();
             }
-            int dp[]=new int[n];
-            int ans=Integer.MIN_VALUE;
-            for(int l=0;l<n;l++){
-                for(int r=l;r<m;r++){
-                    int k=0;
-                    for(int i=0;i<n;i++){
-                        dp[k++]=sum(A,l,r,i);
-                    }
-                    ans=Math.max(ans,kadanes(dp));
-                }
-            }
-            System.out.println(ans);
         }
+        int dp[]=new int[n];
+        int sum=Integer.MIN_VALUE;
+        for(int j=0;j<m;j++){
+            for(int r=j;r<m;r++){
+                for(int i=0;i<n;i++){
+                    dp[i]=sum(A,i,j,r);
+                }
+                sum=Math.max(sum,kadane(dp));
+            }
+        }
+        System.out.println(sum);
     }
 }
