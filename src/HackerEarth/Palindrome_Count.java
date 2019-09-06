b788ed418aa192168e37c7f328c106ddfef3f84b
Kartik <kartikrawat61@gmail.com>
2019-09-06 10:28:44 +0530

    Coding

---
+++ src/HackerEarth/Palindrome_Count.java
@@ -0,0 +1,33 @@
+package HackerEarth;
+
+import java.util.Scanner;
+
+public class Palindrome_Count {
+    public static boolean check(char s[],int l,int r){
+        for(int i=l,j=r;i<j;i++,j--){
+            if(s[i]!=s[j]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static int helper(char s[],int l,int r,Integer dp[][]){
+        if(l>r){
+            return 0;
+        }
+        if(dp[l][r]!=null){
+            return dp[l][r];
+        }
+        if(check(s,l,r)){
+            return dp[l][r]=1+helper(s,l+1,r,dp)+helper(s,l,r-1,dp)-helper(s,l+1,r-1,dp);
+        }else{
+            return dp[l][r]=helper(s,l+1,r,dp)+helper(s,l,r-1,dp)-helper(s,l+1,r-1,dp);
+        }
+    }
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        char s[]=sc.next().toCharArray();
+        Integer dp[][]=new Integer[s.length+1][s.length+1];
+        System.out.println(helper(s,0,s.length-1,dp));
+    }
+}
---
+++ src/HackerEarth/Sell_Computers.java
@@ -0,0 +1,40 @@
+package HackerEarth;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Sell_Computers {
+    public static boolean compare(String a,String b){
+        int n=a.length();
+        int m=b.length();
+        for(int i=0;i<Math.min(n,m);i++){
+            if(b.charAt(i)-'a'<a.charAt(i)-'a'){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        String A[]=new String[n];
+        HashMap<String,Integer> map = new HashMap<>();
+        int max=0;
+        for(int i=0;i<n;i++){
+            A[i]=sc.next();
+            map.put(A[i],map.getOrDefault(A[i],0)+1);
+            max=Math.max(max,map.get(A[i]));
+        }
+        String ans="";
+        for(int i=0;i<A.length;i++){
+            String s=A[i];
+            if(map.get(s)==max){
+                if(ans=="" || compare(ans,s)){
+                    ans=s;
+                }
+            }
+        }
+        System.out.println(ans);
+
+    }
+}
